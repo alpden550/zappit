6f8becf7776aebf48231465b020775c46c588afc
alpden550 <alpden550@gmail.com>
2020-08-05 16:10:37 +0300

    Added post model.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=150)
+    url = models.URLField(max_length=200)
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        verbose_name='User',
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
